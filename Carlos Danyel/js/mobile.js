b5e75713608231848c73a76dcc37299ed905d5a5
Carlos Danyel <carlosdanyelsilva27@gmail.com>
2022-11-02 11:49:03 -0300

    Atualizaçao do responsivo para android

--- Carlos Danyel/js/mobile.js
+++ Carlos Danyel/js/mobile.js
@@ -1,13 +1,12 @@
 const menu = document.querySelector(".header__mobile-menu");
 const nav = document.querySelector(".navbar__container");
-const list = document.querySelector(".header__navbarList");
+const list = document.querySelectorAll(".header__navbar__text");
 
-menu.addEventListener("click", () =>{
+menu.addEventListener("click", navActive )
+
+nav.addEventListener("click", navActive )
+
+function navActive () {
     menu.classList.toggle("active");
     nav.classList.toggle("active");
-});
-
-nav.addEventListener("click", () =>{
-    menu.classList.toggle("active");
-    nav.classList.toggle("active");
-});
+}
